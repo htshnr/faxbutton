1ca856649b0e3993c945c093aba43d645a972e09
Hitesh Nair <htshnr@gmail.com>
2022-02-17 13:10:25 +0530

    checkboxes

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,17 @@ const Home: NextPage = () => {
   // const [showPosts, setShowPosts] = useState(false);
   const [selectedPost, setSelectedPost] = useState({});
 
+  const [destKeys, setDestKeys] = useState(() => {
+    if (typeof window !== "undefined") {
+      const dk = localStorage.getItem("destKeys");
+      if (dk != null) {
+        return JSON.parse(dk);
+      }
+      return {};
+    }
+    return {};
+  })
+
   useEffect(() => {
     const handleEsc = (e: KeyboardEvent) => {
       if (e.key === "Escape") {
@@ -22,6 +33,7 @@ const Home: NextPage = () => {
     };
   }, []);
 
+  console.log("DESTKEYS: ", destKeys)
   return (
     <div className="m-12 flex flex-row justify-center">
       <div className="flex flex-col w-full">
@@ -66,44 +78,174 @@ const Home: NextPage = () => {
       {/* {Object.keys(selectedPost).length !== 0 && <FaxScreen post={selectedPost} />} */}
       {console.log(selectedPost)}
       <div
-        className={`top-0 right-0 w-2/3 bg-blue-700 p-20 text-white fixed overflow-scroll h-full z-10 ease-linear duration-200 ${(Object.keys(selectedPost).length !== 0) ? "translate-x-0 " : "translate-x-full"}`}>
+        className={`top-0 right-0 w-2/3 bg-black p-20 text-white fixed overflow-scroll h-full z-10 ease-in-out duration-200 ${(Object.keys(selectedPost).length !== 0) ? "translate-x-0 " : "translate-x-full"}`}>
 
         <button
           onClick={() => setSelectedPost({})}
-          className="fixed z-20 flex items-center right-10 top-6">
-          Close
+          // className="fixed z-20 flex items-center right-10 top-6"
+          className="relative my-5 text-2xl font-bold "
+          >
+          &#8592;
         </button>
-
+{/* { Object.keys(selectedPost).length !== 0 && */}
         <div>
-          <div className="text-white text-3xl font-semibold">
+          <div className="text-3xl font-semibold">
             Title: {selectedPost.title}
           </div>
-          <div className="text-white mt-2  font-semibold">
+          <div className="mt-2 font-semibold">
             Description : {selectedPost.contentSnippet}
           </div>
-          <div className="text-white mt-2  font-semibold">
-            Source : <a className="underline" href={selectedPost.link}>{selectedPost.link}</a>
+          <div className="mt-2 font-semibold">
+            Source : <a href={selectedPost.link} target="_blank" rel="noopener noreferrer"><span className="underline">{selectedPost.link}</span> &#8599;</a>
           </div>
           {/* <div className="text-white mt-2  font-semibold">
               Content : <p>{selectedPost["content:encodedSnippet"]}</p>
             </div> */}
         </div>
+{/* } */}
+        <div className="mt-12 text-2xl bg-black">
 
-        <div className="my-4 text-2xl">
-          <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
-            <label htmlFor="vehicle1"> Medium</label><br/>
-              <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
-                <label htmlFor="vehicle2"> Dev.to</label><br/>
-                  <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat"/>
-                    <label htmlFor="vehicle3"> Hashnode</label><br/>
-
-                    </div>
-
-                    </div>
-
-
+          <fieldset className="border-t border-b border-white">
+            <legend className="sr-only">Notifications</legend>
+            <div className="divide-y divide-white">
+              <div className="relative flex items-start py-4">
+                <div className="min-w-0 flex-1 text-sm">
+                  <label htmlFor="comments" className="font-medium text-gray-200">
+                    Medium
+                  </label>
+                  {console.log(Boolean(destKeys.Medium))}
+                  {!(Boolean(destKeys.Medium)) &&
+                    <button id="comments-description" className="block text-green-400 hover:text-green-500">
+                      Add your Medium integration token &#8594;
+                    </button>
+                  }
+                </div>
+                <div className="ml-3 flex items-center h-5">
+                  <input
+                    id="comments"
+                    aria-describedby="comments-description"
+                    name="comments"
+                    type="checkbox"
+                    className="focus:ring-green-500 h-4 w-4 text-green-600 border-gray-300 rounded"
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="relative flex items-start py-4">
+                  <div className="min-w-0 flex-1 text-sm">
+                    <label htmlFor="candidates" className="font-medium text-gray-200">
+                      Dev.to
+                    </label>
+                    <p id="candidates-description" className="text-gray-500">
+                      Get notified when a candidate applies for a job.
+                    </p>
                   </div>
-                  )
+                  <div className="ml-3 flex items-center h-5">
+                    <input
+                      id="candidates"
+                      aria-describedby="candidates-description"
+                      name="candidates"
+                      type="checkbox"
+                      className="focus:ring-green-500 h-4 w-4 text-green-600 border-gray-300 rounded"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="relative flex items-start py-4">
+                  <div className="min-w-0 flex-1 text-sm">
+                    <label htmlFor="offers" className="font-medium text-gray-200">
+                      Hashnode
+                    </label>
+                    <p id="offers-description" className="text-gray-500">
+                      Get notified when a candidate accepts or rejects an offer.
+                    </p>
+                  </div>
+                  <div className="ml-3 flex items-center h-5">
+                    <input
+                      id="offers"
+                      aria-describedby="offers-description"
+                      name="offers"
+                      type="checkbox"
+                      className="focus:ring-green-500 h-4 w-4 text-green-600 border-gray-300 rounded"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="relative flex items-start py-4">
+                  <div className="min-w-0 flex-1 text-sm">
+                    <label htmlFor="offers" className="font-medium text-gray-200">
+                      Twitter
+                    </label>
+                    <p id="offers-description" className="text-gray-500">
+                      Get notified when a candidate accepts or rejects an offer.
+                    </p>
+                  </div>
+                  <div className="ml-3 flex items-center h-5">
+                    <input
+                      id="offers"
+                      aria-describedby="offers-description"
+                      name="offers"
+                      type="checkbox"
+                      className="focus:ring-green-500 h-4 w-4 text-green-600 border-gray-300 rounded"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="relative flex items-start py-4">
+                  <div className="min-w-0 flex-1 text-sm">
+                    <label htmlFor="offers" className="font-medium text-gray-200">
+                      Hacker News
+                    </label>
+                    <p id="offers-description" className="text-gray-500">
+                      Get notified when a candidate accepts or rejects an offer.
+                    </p>
+                  </div>
+                  <div className="ml-3 flex items-center h-5">
+                    <input
+                      id="offers"
+                      aria-describedby="offers-description"
+                      name="offers"
+                      type="checkbox"
+                      className="focus:ring-green-500 h-4 w-4 text-green-600 border-gray-300 rounded"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="relative flex items-start py-4">
+                  <div className="min-w-0 flex-1 text-sm">
+                    <label htmlFor="offers" className="font-medium text-gray-200">
+                      Youtube
+                    </label>
+                    <p id="offers-description" className="text-gray-500">
+                      Get notified when a candidate accepts or rejects an offer.
+                    </p>
+                  </div>
+                  <div className="ml-3 flex items-center h-5">
+                    <input
+                      id="offers"
+                      aria-describedby="offers-description"
+                      name="offers"
+                      type="checkbox"
+                      className="focus:ring-green-500 h-4 w-4 text-green-600 border-gray-300 rounded"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+
+        </div>
+
+      </div>
+      
+
+
+    </div>
+  )
 }
 
-                  export default Home
+export default Home
